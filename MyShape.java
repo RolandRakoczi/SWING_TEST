859514faa7f29f1be6bc2be330966326a321938c
Roli <rakocziroland@yahoo.com>
2020-06-29 00:07:45 +0300

    another commit

--- MyShape.java
+++ MyShape.java
@@ -4,9 +4,13 @@ public class MyShape {
     private String name;
     private Shape shape;
 	private int width;
+	private int initWidth;
 	private int height;
+	private int initHeight;
 	private int[] xcoord;
+	private int[] initXcoord;
 	private int[] ycoord;
+	private int[] initYcoord;
 
     public MyShape(String name, Shape shape) {
         this.name = name;
@@ -15,15 +19,27 @@ public class MyShape {
 		if (name.equals("Rectangle")){
 			this.width=(int)shape.getBounds().getWidth();
 			this.height=(int)shape.getBounds().getHeight();
+			this.initWidth=this.width;
+			this.initHeight=this.height;
 		}
     }
-
 
     public Shape getShape() {
         return this.shape;
     }
 	
+	public void init(int[] x,int[] y){
+		initXcoord=new int[3];
+		initYcoord=new int[3];
+		for (int i=0; i<3;i++){
+			this.initXcoord[i]=x[i];
+			this.initYcoord[i]=y[i];
+		}	
+		setSize(initXcoord,initYcoord);
+	}
+	
 	public void setSize(int[] x,int[] y){
+		
 		xcoord = new int[3];
 		ycoord = new int[3];
 		for (int i=0; i<3; i++){
@@ -32,13 +48,52 @@ public class MyShape {
 		}
 	}
 	
+	public void setSize(int x){
+			this.xcoord[0]=this.initXcoord[0];
+			this.ycoord[0]=this.initYcoord[0]-x;
+			this.xcoord[1]=this.initXcoord[1]-x;
+			this.ycoord[1]=this.initYcoord[1]+x;
+			this.xcoord[2]=this.initXcoord[2]+x;
+			this.ycoord[2]=this.initYcoord[2]+x;
+				
+	}
+	
+	public void setSize(int x,int y){
+		this.width=x;
+		this.height=y;
+	}
+	
+	public int getInitWidth(){
+		return this.initWidth;
+	}
+	
+	public int getInitHeight(){
+		return this.initHeight;
+	}
+	
+	public int getWidth(){
+		return this.width;
+	}
+	
+	public int getHeight(){
+		return this.height;
+	}
+	
+	public int[] getInitXcoord(){
+		return initXcoord;
+	}
+	
+	public int[] getInitYcoord(){
+		return initYcoord;
+	}
+	
 	public int[] getXcoord(){
 		return this.xcoord;
 	}
 	
 	public int[] getYcoord(){
 		return this.ycoord;
-	}	
+	}
 
     public String toString() {
         return this.name;
--- SwingTest.java
+++ SwingTest.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.event.*;
+import javax.swing.border.LineBorder;
 
 public class SwingTest extends JFrame implements ActionListener {
     private JPanel panel;
@@ -9,16 +11,17 @@ public class SwingTest extends JFrame implements ActionListener {
     private JComboBox<MyShape> jcomboShape;
     private JComboBox<MyColor> jcomboColor;
     private JCheckBox checkbox;
+	private JScrollPane scrollpane;
     private JButton button;
+	private JSlider slider;
     private MyShape[] shape;
     private MyColor[] colors;
 
 
     public SwingTest() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        panel = new JPanel();
-        this.add(panel);
+		this.setSize(500,550);
+		this.setLayout(null);
 		
 		colors=new MyColor[5];
         colors[0]=new MyColor("red", Color.red);
@@ -28,11 +31,15 @@ public class SwingTest extends JFrame implements ActionListener {
         colors[4]=new MyColor("yellow", Color.yellow);
 		
 		jcomboColor=new JComboBox<MyColor>(colors);	
+		jcomboColor.setMaximumSize(new Dimension(120,50));
+		jcomboColor.setAlignmentX(CENTER_ALIGNMENT);
 
         subpanel = new JPanel();
-        subpanel.setPreferredSize(new Dimension(500, 300));
+		subpanel.setBorder(LineBorder.createBlackLineBorder());
         subpanel.setBackground(Color.gray);
-		panel.add(subpanel, BorderLayout.CENTER);
+		scrollpane=new JScrollPane(subpanel,ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollpane.setBounds(5,5,475,300);
+		this.add(scrollpane);
 
 		shape = new MyShape[2];
         Shape shape1 = new Rectangle(200, 100);
@@ -41,26 +48,41 @@ public class SwingTest extends JFrame implements ActionListener {
         shape[1] = new MyShape("Triangle",shape2);	
 		
 		jcomboShape = new JComboBox<MyShape>(shape);
+		jcomboShape.setMaximumSize(new Dimension(120,50));
+		jcomboShape.setAlignmentX(CENTER_ALIGNMENT);
 			
         subpanel2 = new JPanel();
+		subpanel2.setBounds(5,305,475,205);
+		subpanel2.setAlignmentX(CENTER_ALIGNMENT);
         subpanel2.setLayout(new BoxLayout(subpanel2, BoxLayout.Y_AXIS));
+		slider=new JSlider(JSlider.HORIZONTAL,-50,300,0);
+		slider.setMajorTickSpacing(50);
+		slider.setMinorTickSpacing(10);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		slider.setAlignmentX(CENTER_ALIGNMENT);
+		slider.addChangeListener(new SliderListener());
         subpanel2.add(Box.createRigidArea(new Dimension(0, 10)));
+		subpanel2.add(slider);
+		subpanel2.add(Box.createRigidArea(new Dimension(0,5)));
         subpanel2.add(jcomboColor);
         subpanel2.add(Box.createRigidArea(new Dimension(0, 5)));
         subpanel2.add(jcomboShape);
         subpanel2.add(Box.createRigidArea(new Dimension(0, 5)));
         checkbox = new JCheckBox("fill out");
+		checkbox.setAlignmentX(CENTER_ALIGNMENT);
         subpanel2.add(checkbox);
         subpanel2.add(Box.createRigidArea(new Dimension(0, 5)));
         button = new JButton("Button");
+		button.setAlignmentX(CENTER_ALIGNMENT);
         button.addActionListener(this);
         subpanel2.add(button);
-        panel.add(subpanel2, BorderLayout.SOUTH);
+        this.add(subpanel2);
 		
-		int[]x ={(int)(subpanel.getPreferredSize().getWidth()/2),(int)(subpanel.getPreferredSize().getWidth()/3),(int)(subpanel.getPreferredSize().getWidth()*2/3)};
-		int[]y ={(int)(subpanel.getPreferredSize().getHeight()/3),(int)(subpanel.getPreferredSize().getHeight()*2/3),(int)(subpanel.getPreferredSize().getHeight()*2/3)};
+		int[]x ={(int)(scrollpane.getBounds().getWidth()/2),(int)(scrollpane.getBounds().getWidth()/3),(int)(scrollpane.getBounds().getWidth()*2/3)};
+		int[]y ={(int)(scrollpane.getBounds().getHeight()/3)+35,(int)(scrollpane.getBounds().getHeight()*2/3)+35,(int)(scrollpane.getBounds().getHeight()*2/3)+35};
 
-		shape[1].setSize(x,y);
+		shape[1].init(x,y);
 
     }
 
@@ -73,12 +95,13 @@ public class SwingTest extends JFrame implements ActionListener {
         MyShape s = (MyShape) jcomboShape.getSelectedItem();
 		
 		if (s.toString().equals("Rectangle")){
-			Rectangle rect = s.getShape().getBounds();
-        
+			int sum=(int) (subpanel.getHeight()) / 2 -(int) (s.getHeight()/2);
+			System.out.println("Subpanel height="+subpanel.getHeight()+" Rectangle height="+s.getHeight()+" Form="+sum);
+			System.out.println("Subpanel coords : x="+subpanel.getBounds().getX()+" y="+subpanel.getBounds().getY());
 			if (checkbox.isSelected()) {
-				g.fillRect((int) (subpanel.getWidth()) / 2 - (int) (rect.getWidth() / 2), (int) (subpanel.getHeight()) / 2, (int) rect.getWidth(), (int) rect.getHeight());
+				g.fillRect((int) (scrollpane.getBounds().getWidth()) / 2 - (int) (s.getWidth() / 2)+12, (int) (scrollpane.getBounds().getHeight()) / 2 -(int) (s.getHeight()/2.5-4), (int) s.getWidth(), (int) s.getHeight());
 				} else {
-					g.drawRect((int) (subpanel.getWidth()) / 2 - (int) (rect.getWidth() / 2), (int) (subpanel.getHeight()) / 2, (int) rect.getWidth(), (int) rect.getHeight());
+					g.drawRect((int) (scrollpane.getBounds().getWidth()) / 2 - (int) (s.getWidth() / 2)+12, (int) (scrollpane.getBounds().getHeight()) / 2-(int) (s.getHeight()/2.5-4), (int) s.getWidth(), (int) s.getHeight());
 				}
 			}			
 		
@@ -96,11 +119,26 @@ public class SwingTest extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         repaint();
     }
+		
+	class SliderListener implements ChangeListener{
+		public void stateChanged(ChangeEvent e){
+			for (int i=0;i<shape.length;i++){
+				//System.out.println(s.toString());
+				if (shape[i].toString().equals("Rectangle")){
+					//System.out.println("Slider value="+slider.getValue()+" X="+shape[i].getWidth()+" Y="+shape[i].getHeight());
+					shape[i].setSize(shape[i].getInitWidth()+slider.getValue(),shape[i].getInitHeight()+slider.getValue());
+					repaint();
+				} else {
+					shape[i].setSize(slider.getValue());
+				}
+			}
+		}		
+	}		
 
     public static void main(String[] args) {
         SwingTest test = new SwingTest();
-        test.setBounds(50, 50, 500, 500);
         test.setVisible(true);
+		test.setLocationRelativeTo(null);
         test.setResizable(false);
     }
 }
